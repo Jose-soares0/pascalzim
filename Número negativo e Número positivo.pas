9447d67055a4a3df746eb7ee70ebea0f304eddbb
josé soares da silva <josesoaresdasilva006@gmail.com>
2025-04-03 16:22:44 -0300

    Código para descobrir se um número é negativo ou positivo

---
+++ Número negativo e Número positivo.pas
@@ -0,0 +1,11 @@
+Program Positivo_ou_negativo ;
+var numero:integer;
+Begin
+ writeln ('Informe um número');
+ read(numero);
+ if (numero>=0) then
+ writeln(numero, ' É positivo')
+ else 
+ writeln(numero, ' É negativo');
+  
+End.
